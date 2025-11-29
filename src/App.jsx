034660369489f36cf8f332c9baf34803aa01d66a
phalla <landphalll@gmail.com>
2025-11-29 20:44:48 +0700

    make navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,14 +2,13 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Navbar from "./components/Navbar";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
-    <>
-      <h1 className="text-red-400">hello</h1>
-    </>
+   <Navbar/>
   );
 }
 
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,39 @@
 import React from 'react'
-
+import logo2 from '../assets/logo2.png'
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <header className='w-full bg-[#F9F8F6] px-20 py-8'>
+        <div className='flex items-center justify-between '>
+            <div className='w-40 relative'>
+              <img className='w-full absolute translate-y-[-49%]' src={logo2} alt="" />
+            </div>
+            <ul className='flex gap-8'>
+              <li>Home</li>
+              <li>About us</li>
+              <li>Products</li>
+              <li>FAQ</li>
+              <li>Contact us</li>
+            </ul>
+
+            <div className='flex text-center gap-10'>
+               <div className='max-w-md relative'>
+              <input type="text" placeholder='Search' className='w-full border border-green-800 px-5 rounded-3xl py-1' />
+              <i className='bi bi-search absolute translate-1.5 translate-x-[-175%] text-[14px]'></i>
+            </div>
+            <div className=' flex gap-1.5'>
+              <div className='w-8 h-8 border border-green-900 rounded-[50%] flex items-center justify-center' >
+                <i class="bi bi-cart text-green-950 text-[10px]"></i>
+              </div>
+              <div className=' w-35 h-8 bg-green-900 flex gap-1 items-center justify-center rounded-2xl '>
+                <i class="bi bi-telephone-fill text-white text-[12px]"></i>
+                <p className='text-white text-[12px]'> +885 962657233</p>
+              </div>
+              </div>
+            </div>
+        </div>
+      </header>
+    </div>
   )
 }
 
