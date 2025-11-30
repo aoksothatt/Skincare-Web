c50b9a569746d0734876f6e7a339d6cd40a35e2c
phalla <landphalll@gmail.com>
2025-11-30 22:38:11 +0700

    Link-router

--- src/Pages/WelcomePage.jsx
+++ src/Pages/WelcomePage.jsx
@@ -2,18 +2,20 @@ import React from "react";
 import form from "../assets/form.png"
 const WelcomePage = () => {
   return <div>
-    <div className="w-full h-screen bg-[#F9F8F6] py-35 px-25">
+    <div className="w-full h-screen py-35 px-25">
       <div className=" flex justify-between">
         <div className="w-[25%]">
           <h2 className="text-6xl leading-15 "> <span className="flex gap-4"> Glow <img className="w-40 my-auto h-15 rounded-4xl" src="https://media.istockphoto.com/id/2182600309/photo/beauty-portraits-of-beautiful-young-asian-women.jpg?s=612x612&w=0&k=20&c=CNLoIjsY-YHu0sF82lixz_CQYiVgjXJnb1Z0yWoXcdc=" alt="" /> </span> 
           <span className=" text-green-800">Naturally </span>Every Day</h2>
-          <button className=" mt-10 border border-green-900 w-55 h-13 rounded-xl flex items-center justify-center gap-2">View All Product <i class="bi bi-arrow-up-right-square-fill text-green-800 text-[42px]"></i></button>
+          <button className=" mt-10 border border-green-900 w-55 h-13 rounded-xl flex items-center justify-center gap-2">View All Product <i className="bi bi-arrow-up-right-square-fill text-green-800 text-[42px]"></i></button>
           <div className="mt-20">
-            <p className="w-60 font-bold text-lg relative">Make with<span className="text-5xl text-green-800 absolute translate-y-[-23%] translate-x-2"> 99%</span> 
-            <p><strong>Natural Ingredient</strong> for Pure,Healthy Skin.</p></p>
+            <div className="w-60 font-bold text-lg relative">Make with<span className="text-5xl text-green-800 absolute translate-y-[-23%] translate-x-2">99%</span>  <div>
+              <strong>Natural Ingredient</strong> for Pure, Healthy Skin.
+            </div>
+            </div>
           </div>
         </div>
-        <div className="w-[40%] h-[450px] bg-[#84994F] rounded-[500px_90px] relative ">
+        <div className="w-[40%] h-[450px] bg-[#5E936C] rounded-[500px_90px] relative ">
           <img className="w-full absolute bottom-8 auto-scroll" src={form} alt="" />
           <h2 className="absolute top-[-19%] auto-scroll left-88.5   bg-green-800 text-white font-bold py-2 px-3 rounded-3xl">New Product</h2>
         </div>
--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,4 +1,5 @@
 import React from 'react'
+import { Link } from "react-router-dom";
 import logo2 from '../assets/logo2.png'
 const Navbar = () => {
   return (
@@ -9,21 +10,28 @@ const Navbar = () => {
               <img className='w-full absolute translate-y-[-49%]' src={logo2} alt="" />
             </div>
             <ul className='flex gap-8'>
-              <li>Home</li>
-              <li>About us</li>
-              <li>Products</li>
-              <li>FAQ</li>
-              <li>Contact us</li>
+              <li>
+                <Link to="/">Home</Link>
+              </li>
+              <li>
+                <Link to="/about">About us</Link>
+              </li>
+              <li>
+                <Link to="/products">Products</Link>
+              </li>
+              <li>
+                <Link to="/contact">Contact us</Link>
+              </li>
             </ul>
 
             <div className='flex text-center gap-10'>
               
             <div className=' flex gap-1.5'>
               <div className='w-8 h-8 border border-green-900 rounded-[50%] flex items-center justify-center' >
-                <i class="bi bi-cart text-green-950 text-[10px]"></i>
+                <i className="bi bi-cart text-green-950 text-[10px]"></i>
               </div>
               <div className=' w-35 h-8 bg-green-900 flex gap-1 items-center justify-center rounded-2xl '>
-                <i class="bi bi-telephone-fill text-white text-[12px]"></i>
+                <i className="bi bi-telephone-fill text-white text-[12px]"></i>
                 <p className='text-white text-[12px]'> +885 962657233</p>
               </div>
               </div>
